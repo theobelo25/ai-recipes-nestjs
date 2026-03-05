6e60dac495ac252b6890b7e7bcbc68994558dec0
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-03-05 12:57:07 -0500

    healthcheck is public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './domain/auth';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getRoot() {
     return this.appService.getRoot();
   }
